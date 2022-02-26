c3997660cc8280bea55e04a4a52eb37f37a21a88
Eduardo Rauta <ebrauta@gmail.com>
2022-02-26 16:08:21 -0300

    Versão 2.0

--- projeto.js
+++ projeto.js
@@ -1,158 +1,161 @@
+class Produto {
+  constructor(name, price, qtde, icon) {
+    this.name = name;
+    this.price = price;
+    this.qtde = qtde;
+    this.icon = icon;
+  }
+  getName() {
+    return this.name;
+  }
+
+  getPrice() {
+    return this.price;
+  }
+
+  getQtde() {
+    return this.qtde;
+  }
+
+  getIcon() {
+    return this.icon;
+  }
+}
+
+const toCurrency = function (value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+};
+
 const listaDeProdutos = [
-    {
-        'name': 'CPU',
-        'price': 350
-    },
-    {
-        'name': 'Monitor',
-        'price': 400
-    },
-    {
-        'name': 'Teclado',
-        'price': 120
-    },
-    {
-        'name': 'Mouse',
-        'price': 30
-    },
-    {
-        'name': 'Caixa de Som',
-        'price': 75
-    },
-    {
-        'name': 'Impressora',
-        'price': 300
-    }
+  new Produto("CPU", 350, 0, "memory"),
+  new Produto("Monitor", 400, 0, "monitor"),
+  new Produto("Teclado", 120, 0, "keyboard"),
+  new Produto("Mouse", 30, 0, "mouse"),
+  new Produto("Caixa de Som", 75, 0, "speaker"),
+  new Produto("Impressora", 300, 0, "adf_scanner"),
+  new Produto("Roteador", 150, 0, "router"),
 ];
 
-const raiz = document.getElementById('root')
+const raiz = document.getElementById("root");
+const lado = document.getElementById("side");
 
-var produtos = []
+const createInput = (type, id, value, action, title) => {
+  let inputItem = document.createElement("input");
+  inputItem.setAttribute("type", type);
+  inputItem.setAttribute("value", value);
+  if (type === "number") {
+    inputItem.setAttribute("id", id);
+    inputItem.setAttribute("min", 0);
+  } else if (type === "button") {
+    inputItem.addEventListener("click", action);
+    inputItem.setAttribute("class", id);
+    inputItem.title = title;
+  }
+  return inputItem;
+};
 
-let ulist = document.createElement('ul')
+const productsList = document.createElement("ul");
 listaDeProdutos.forEach((item, index) => {
-    let lItem = document.createElement('li')
-    let sItem = document.createElement('span')
-    sItem.setAttribute('id', `res${index}`)
-    let valor =  Number(item.price).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'});
-    sItem.innerText = `${item.name} - ${valor}`
-    lItem.appendChild(sItem)
-    let nItem = document.createElement('input')
-    nItem.setAttribute('type', 'number')
-    nItem.setAttribute('id',`qtde${index}`)
-    nItem.setAttribute('min', 0)
-    nItem.setAttribute('value', 0)
-    lItem.appendChild(nItem)
-    let bitem = document.createElement('input')
-    bitem.setAttribute('type', 'button')
-    bitem.setAttribute('value', 'Comprar')
-    bitem.addEventListener('click', () => insereNaLista(item, nItem.value))
-    lItem.appendChild(bitem)
-    let b2item = document.createElement('input')
-    b2item.setAttribute('type', 'button')
-    b2item.setAttribute('value', 'Retirar')
-    b2item.addEventListener('click', () => retiraDaLista(item, nItem.value))
-    lItem.appendChild(b2item)
-    ulist.appendChild(lItem)
-})
+  let listItem = document.createElement("li");
+  let spanItem = document.createElement("span");
+  spanItem.setAttribute("id", `res${index}`);
+  spanItem.classList.add("boxProduct");
+  spanItem.innerHTML = `<span class="material-icons md-48">${item.getIcon()}</span><span>${item.getName()}</span><span>${toCurrency(
+    item.getPrice()
+  )}</span>`;
+  listItem.appendChild(spanItem);
 
-raiz.appendChild(ulist)
+  let buttonGroup = document.createElement("div");
+  buttonGroup.classList.add("btngroup");
 
-const insereNaLista = (produto, qtde) => {
-    if(qtde !== '0'){
-        if(produtos.indexOf(produto) < 0){
-            let newProduto = {
-                'name': produto.name,
-                'price': produto.price,
-                'qtde': Number(qtde)
-            }
-            produtos.push(newProduto)
-            let total = produtos.reduce((total, item) => {
-                return total + (item.price * item.qtde)
-            }, 0)
-            console.log(total)
-        }
-    } else {
-        alert('Insira a quantidade do produto')
+  let buttonInsert = createInput(
+    "button",
+    "btn",
+    "shopping_cart",
+    () => insereNaLista(item),
+    "Comprar"
+  );
+  buttonInsert.classList.add("insert");
+  buttonInsert.classList.add("material-icons");
+  buttonGroup.appendChild(buttonInsert);
+
+  let buttonDelete = createInput(
+    "button",
+    "btn",
+    "delete",
+    () => retiraDaLista(item),
+    "Apagar"
+  );
+  buttonDelete.classList.add("delete");
+  buttonDelete.classList.add("material-icons");
+  buttonGroup.appendChild(buttonDelete);
+  listItem.appendChild(buttonGroup);
+  productsList.appendChild(listItem);
+});
+
+raiz.appendChild(productsList);
+
+const divList = document.createElement("ul");
+lado.appendChild(divList);
+
+const buttonClearList = createInput(
+  "button",
+  "clearAll",
+  "delete_sweep",
+  () => limparCarrinho(),
+  "Limpar lista"
+);
+buttonClearList.classList.add("delete");
+buttonClearList.classList.add("material-icons");
+
+lado.appendChild(buttonClearList);
+
+const insereNaLista = (produto) => {
+  let prod = listaDeProdutos.find((item) => item == produto);
+  prod.qtde += 1;
+  let total = calculaTotal();
+  resultado.innerHTML = `Total:  ${toCurrency(total)}`;
+  mostraLista();
+};
+
+const retiraDaLista = (produto) => {
+  let prod = listaDeProdutos.find((item) => item == produto);
+  let index = listaDeProdutos.indexOf(prod);
+  prod.qtde !== 0
+    ? (prod.qtde -= 1)
+    : alert("Não tem mais este produto na lista!");
+  resultado.innerHTML = `Total:  ${toCurrency(calculaTotal())}`;
+  mostraLista();
+};
+
+const calculaTotal = () => {
+  return listaDeProdutos.reduce((total, item) => {
+    return total + Number(item.getPrice()) * Number(item.getQtde());
+  }, 0);
+};
+
+const mostraLista = () => {
+  while (divList.firstChild) {
+    divList.removeChild(divList.firstChild);
+  }
+  listaDeProdutos.forEach((item) => {
+    if (Number(item.getQtde()) !== 0) {
+      let prodItem = document.createElement("li");
+      prodItem.setAttribute("id", item.getName());
+      prodItem.innerHTML = `${item.getQtde()}\t\t${item.getName()}`;
+      divList.appendChild(prodItem);
     }
-}
+  });
+  lado.appendChild(divList);
+};
 
-const retiraDaLista = (produto, qtde) => {
-    let newProduto = {
-        'name': produto.name,
-        'price': produto.price,
-        'qtde': Number(qtde)
-    }
-    produtos = produtos.filter((item) => {item !== newProduto})
-    let total = produtos
-    console.log(total)
-}
-
-/*
- <li id='produto1'><span id="res1">Monitor - R$ 400,00 </span>Qtde: <input type='number' id='qtde1' min="0" value=0><input type="button" value="Comprar" onclick="clicar(1)"> </li>
-        <li id='produto2'><span id="res2">CPU - R$ 350,00 </span>Qtde: <input type='number' id='qtde2' min="0" value=0><input type="button" value="Comprar" onclick="clicar(2)"> </li>
-        <li id='produto3'><span id="res3">Teclado - R$ 120,00 </span>Qtde: <input type='number' id='qtde3' min="0" value=0><input type="button" value="Comprar" onclick="clicar(3)"> </li>
-        <li id='produto4'><span id="res4">Mouse - R$ 25,00 </span>Qtde: <input type='number' id='qtde4' min="0" value=0><input type="button" value="Comprar" onclick="clicar(4)"> </li>
-        <li id='produto5'><span id="res5">Caixa de Som - R$ 75,00 </span>Qtde: <input type='number' id='qtde5' min="0" value=0><input type="button" value="Comprar" onclick="clicar(5)"> </li>
-        <li id='produto6'><span id="res6">Impressora - R$ 300,00 </span>Qtde: <input type='number' id='qtde6' min="0" value=0><input type="button" value="Comprar" onclick="clicar(6)"> </li>
-
-
-
-        Total: <span id="totValor"></span>
-    */
-/*
-function insereNaLista(item, qtde){
-    let divprod = document.getElementById('lista')
-    let divtotal = document.getElementById('totValor')
-    try {
-        item = item.split(' - ')
-        prodName = item[0]
-        prodValor = Number(item[1].slice(2).replace(',','.')) * qtde;
-        let prods = {name: prodName, value: prodValor}
-        produtos.push(prods);
-        let prodItem;
-        let buttonItem;
-        produtos.forEach((value) => {
-            prodItem = document.createElement('p')
-            buttonItem = document.createElement('input')
-            let valor =  Number(value.value).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'});
-            prodItem.innerHTML = `${value.name} - ${qtde} peça(s) = ${valor}`
-            buttonItem.type = 'button'
-            buttonItem.value = 'Retirar'
-            buttonItem.addEventListener('click', () => retiraDaLista(value))
-            prodItem.appendChild(buttonItem)
-        }) 
-        var totalFinal = produtos.reduce((total, value) => total += value.value, 0);
-        divprod.appendChild(prodItem);
-        divtotal.innerHTML = Number(totalFinal).toLocaleString('pt-BR',{style: 'currency', currency:'BRL'});
-        alert('Compra efetuada com sucesso')
-    } catch (error) {
-        alert(`Erro: ${error} \nSua compra foi cancelada`)     
-    } 
-}
-
-function retiraDaLista(produto){
-    let index = produtos.indexOf(produto) + 1
-    console.log(index)
-    //console.log(document.getElementById('produto'+i))
-    if(produtos.length === 0){
-        alert('Não há produtos na lista')
-    } else {
-        //document.getElementById('prod'+i).style.display = 'block' 
-        //produtos.splice(i, 1);
-    }
-    console.log(produtos)
-}
-
-function clicar(i){
-    let prod = document.getElementById('res'+i);
-    let qtd = document.getElementById('qtde'+i);
-    if(qtd.value <= 0){
-        alert('Insira a quantidade de produtos pedidos')
-    }
-    else{  
-        document.getElementById('produto'+i).style.display = 'none'    
-        insereNaLista(prod.innerHTML, qtd.value); 
-    }
-}
-*/
+const limparCarrinho = () => {
+  while (divList.firstChild) {
+    divList.removeChild(divList.firstChild);
+  }
+  listaDeProdutos.forEach((item) => (item.qtde = 0));
+  resultado.innerHTML = `Total:  ${toCurrency(calculaTotal())}`;
+};
